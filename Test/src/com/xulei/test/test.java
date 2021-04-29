38697c1ac04d9ec68bdbe984a45b1d43a91cc464
XuLeiiii <879627401@qq.com>
2021-04-29 23:58:05 +0800

    first3 commit

---
+++ Test/src/com/xulei/test/test.java
@@ -0,0 +1,18 @@
+package com.xulei.test;
+
+/**
+ * @author xl
+ * @Description:
+ * @date: 2021-04-29 23:54
+ * @since JDK 1.8
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("4444");
+        System.out.println("3333");
+        System.out.println("2222");
+        System.out.println("2222");
+        System.out.println("2222");
+    }
+}
