86b8b4c5e04aebd814cd5ee4ff1bb8861a766e3f
XuLeiiii <879627401@qq.com>
2021-04-29 23:59:44 +0800

    4

--- Test/src/com/xulei/test/test.java
+++ Test/src/com/xulei/test/test.java
@@ -9,10 +9,8 @@ package com.xulei.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("1111");
-        System.out.println("4444");
-        System.out.println("3333");
-        System.out.println("2222");
-        System.out.println("2222");
-        System.out.println("2222");
+        System.out.println("333");
+        System.out.println("444");
+        System.out.println("555");
     }
 }
